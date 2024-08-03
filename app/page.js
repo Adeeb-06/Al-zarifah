b22e474e4ab92d333cfa49b0139e602149997ed9
specs-wire <specswire@gmail.com>
2024-08-03 23:56:19 +0600

    final

--- app/page.js
+++ app/page.js
@@ -1,13 +1,10 @@
 import About from "@/components/About";
 import Brand from "@/components/Brand";
-import Client from "@/components/Client";
-import Contact from "@/components/Contact";
 import Feature from "@/components/Feature";
 import Hajj from "@/components/Hajj";
 import Hero from "@/components/Hero";
 import SearchPanel from "@/components/SearchPanel";
 import Top from "@/components/Top";
-import Image from "next/image";
 
 export default function Home() {
   return (
